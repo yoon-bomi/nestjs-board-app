2ddf00a2df309934e447ed9c1e67f1c927e783b7
Windy <ydg0330@naver.com>
2022-05-15 15:40:05 +0900

    feat : 기존 service DI

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,11 @@
 import { Controller } from '@nestjs/common';
+import { BoardsService } from './boards.service';
 
 @Controller('boards')
-export class BoardsController {}
+export class BoardsController {
+  boardsService: BoardsService;
+
+  constructor(boardsService: BoardsService) {
+    this.boardsService = boardsService;
+  }
+}
