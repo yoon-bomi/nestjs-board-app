275da8e46359349cca3b4478a3eab012e21914e7
Windy <ydg0330@naver.com>
2022-05-21 22:36:38 +0900

    chore : 현재 권장 typeorm 버전은 0.2.xx

--- src/boards/board.repository.ts
+++ src/boards/board.repository.ts
@@ -1,6 +1,5 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { Board } from './board.entity';
 
-//FIXME: @EntityRepository is deprecated
 @EntityRepository(Board)
 export class BoardRepository extends Repository<Board> {}
