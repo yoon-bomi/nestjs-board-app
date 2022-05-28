a6ffcc6465f54cb7f708cdb2d4e4f46db5dc001b
Windy <ydg0330@naver.com>
2022-05-28 22:18:47 +0900

    feat : createBoard service method

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -2,12 +2,28 @@ import { Injectable } from '@nestjs/common';
 import { BoardRepository } from './board.repository';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Board } from './board.entity';
+import { CreateBoardDto } from './dto/create-board-dto';
+import { BoardStatus } from './board-status.enum';
 @Injectable()
 export class BoardsService {
   constructor(
     @InjectRepository(BoardRepository)
     private boardRepository: BoardRepository,
   ) {}
+
+  async createBoard(createBoard: CreateBoardDto): Promise<Board> {
+    const { title, description } = createBoard;
+
+    const board = this.boardRepository.create({
+      title,
+      description,
+      status: BoardStatus.PUBLIC,
+    });
+
+    await this.boardRepository.save(board);
+
+    return board;
+  }
 
   async getBoardById(id: number): Promise<Board> {
     const board = await this.boardRepository.findOneOrFail({
