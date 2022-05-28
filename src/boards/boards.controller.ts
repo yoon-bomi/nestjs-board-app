c70bdd6ae997721f03b8a0c325d7bcee0e39fd9d
Windy <ydg0330@naver.com>
2022-05-28 22:19:07 +0900

    feat : createBoard controller method

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,6 +1,15 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { Board } from './board.entity';
 import { BoardsService } from './boards.service';
+import { CreateBoardDto } from './dto/create-board-dto';
 
 @Controller('boards')
 export class BoardsController {
@@ -9,6 +18,12 @@ export class BoardsController {
   @Get('/:id')
   getBoardById(@Param('id') id: number): Promise<Board> {
     return this.boardsService.getBoardById(id);
+  }
+
+  @Post()
+  @UsePipes(ValidationPipe)
+  createBoard(@Body() createBoardDto: CreateBoardDto): Promise<Board> {
+    return this.boardsService.createBoard(createBoardDto);
   }
 
   // NOTE: Patch: 일부를 업데이트, Put: 전체를 업데이트
